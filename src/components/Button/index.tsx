170e2fd6a5652dc1c07e890eb5d75f0907a12c98
dawanlago <dawanlago1@gmail.com>
2024-10-11 12:44:22 -0300

    first commit

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Button: React.FC = () => {
+  return (
+    <button className="mt-4 px-4 py-2 font-bold bg-orange-500 text-white rounded">
+      Botão
+    </button>
+  );
+};
+
+export default Button;
